ad03412ec0ac6245c47e402a99201848246d704c
niutengfei <niutengfei@magiccompass.cn>
2026-01-08 17:41:59 +0800

    refactor: optimize cube positioning and camera setup in Three.js application

--- app.js
+++ app.js
@@ -4,16 +4,27 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
-cube.position.x = 1;
-cube.position.y = -2;
-cube.position.z = 0.5;
+// cube.position.x = 0.7;
+// cube.position.y = -0.6;
+// cube.position.z = 1;
+cube.position.set(0.7, -0.6, 1);
+cube.scale.set(2, 0.5, 0.5);
+cube.rotation.reorder('YXZ');
+cube.rotation.y = Math.PI / 2
+cube.rotation.x = Math.PI / 4;
+// cube.position.normalize();
 scene.add(cube);
+
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
 const size = {
     width: 800,
     height: 600
 };
-const camera = new THREE.PerspectiveCamera(75, size.width / size.height, 0.1, 1000);
-camera.position.z = 5;
+const camera = new THREE.PerspectiveCamera(45, size.width / size.height);
+camera.position.set(1, 1, 8);
+camera.lookAt(cube.position);
+console.log(cube.position.distanceTo(camera.position));
 const canvasDom = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({ canvas: canvasDom });
 renderer.setSize(size.width, size.height);
