e3eeac980eb34a157f17727b51ce77de839ad412
niutengfei <niutengfei@magiccompass.cn>
2026-01-09 15:14:23 +0800

    refactor: update cube geometry and camera settings in Three.js scene

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
-import gsap from 'gsap';
 const scene = new THREE.Scene();
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(1, 1, 1, 5, 5, 5);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 
@@ -10,29 +9,30 @@ const size = {
     width: 800,
     height: 600
 };
-const camera = new THREE.PerspectiveCamera(45, size.width / size.height, 0.1, 1000);
-camera.position.z = 5;
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height, 0.1, 100);
+// const aspectRatio = size.width / size.height;
+// const camera = new THREE.OrthographicCamera(
+//     -1 * aspectRatio,
+//     1 * aspectRatio,
+//     -1,
+//     1,
+//     0.1,
+//     100
+// );
+camera.position.z = 2;
+camera.position.x = 2;
+camera.position.y = 2;
+camera.lookAt(cube.position);
 const canvasDom = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({ canvas: canvasDom });
 renderer.setSize(size.width, size.height);
 
-// const time = Date.now();
-// const tick = () => {
-//     const currentTime = Date.now();
-//     const deltaTime = currentTime - time;   
-//     cube.rotation.y = 0.001 * deltaTime;
-//     renderer.render(scene, camera);
-//     requestAnimationFrame(tick);
-// }
-// tick();
-gsap.to(cube.position, { duration: 1, delay: 1, x: 2 });
-gsap.to(cube.position, { duration: 1, delay: 2, x: 0 });
 const clock = new THREE.Clock();
 const tick = () => {
-    // const elapsedTime = clock.getElapsedTime();
-    // // cube.rotation.y = elapsedTime;
+    const elapsedTime = clock.getElapsedTime();
+    cube.rotation.y = elapsedTime;
     // cube.position.y = Math.sin(elapsedTime);
-    // // cube.rotation.y = elapsedTime;
+    // cube.rotation.y = elapsedTime;
     // cube.position.x = Math.cos(elapsedTime);
     renderer.render(scene, camera);
     requestAnimationFrame(tick);
